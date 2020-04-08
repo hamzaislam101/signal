fc3609b12859f59430c6fbaaa09035b81bc13c4c
hamzaislam101 <hamzaislam101@gmail.com>
2020-04-08 12:30:18 +0500

    initial commit

---
+++ signal/Pages/ChartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace signal.Pages
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChartController : ControllerBase
+    {
+        private IHubContext<ChatHub> _hub;
+
+        public ChartController(IHubContext<ChatHub> hub)
+        {
+            _hub = hub;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _hub.Clients.All.SendAsync("showMessage");
+            return Ok();
+        }
+    }
+}
